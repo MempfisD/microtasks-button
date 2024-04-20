a36a2d7ec9976c9c54c2fc0f47d66c1b5a21cff0
Dmitry Naumov <komrad.nda@yandex.ru>
2024-04-20 15:04:19 +0300

    MouseEvent add

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {MouseEvent} from 'react'
+import './App.css'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	// const myFirstSubscriber = (event: MouseEvent<HTMLButtonElement>) => {
+	// 	console.log('Hello I`m Vasya!')
+	// }
+	//
+	// const mySecondSubsciber = (event: MouseEvent<HTMLButtonElement>) => {
+	// 	console.log('Hello I`m Ivan!')
+	// }
+
+	const onClickHandler = (name: string) => {
+		console.log(name)
+	}
+
+
+	return (
+			<div className="App">
+				{/* <button
+				onClick={event => {
+					console.log('Hello')
+				}}
+			>
+				MyYouTubeChanel-1
+			</button> */}
+				<button
+						onClick={(event: MouseEvent<HTMLButtonElement>) =>
+								onClickHandler('Vasya!')
+						}
+				>
+					MyYouTubeChanel-1
+				</button>
+				<button
+						onClick={(event: MouseEvent<HTMLButtonElement>) =>
+								onClickHandler('Ivan!')
+						}
+				>
+					MyYouTubeChanel-2
+				</button>
+
+			</div>
+
+	)
 }
 
-export default App;
+export default App
